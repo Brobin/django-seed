29922785b744129eae62fbbda7ef66e5f6eb29be
Paul Shannon <paul@paulshannon.ca>
2015-06-26 08:43:26 -0400

    Added more flexible applist argument Changed to argparse from optparse

--- django_seed/management/commands/seed.py
+++ django_seed/management/commands/seed.py
@@ -1,40 +1,72 @@
-
-from django.core.management.base import AppCommand
+from django.apps import apps
+from django.core.management.base import BaseCommand
 from django_seed import Seed
 from django_seed.exceptions import SeederCommandError
-from optparse import make_option
-import django
 
 
-class Command(AppCommand):
+class Command(BaseCommand):
     help = 'Seed your Django database with fake data'
 
-    args = "[appname ...]"
+    def add_arguments(self, parser):
+        parser.add_argument('args', metavar='app_label[.ModelName]', nargs='*',
+                            help='Restricts seeding to the specified app_label or app_label.ModelName.')
+        parser.add_argument('-a', '--all', action='store_true', dest='seed_all', default=False,
+                            help="Seed all registered apps.")
+        parser.add_argument('-n', '--number', type=int, default=10, help="Number of each model to seed.")
 
-    option_list = AppCommand.option_list + (
-        make_option('--number', dest='number', default=10,
-                    help='number of each model to seed'),
-    )
+    def add_model(self, model):
+        if model not in self.models:
+            self.models.append(model)
 
-    def handle_app_config(self, app_config, **options):
-        if app_config.models_module is None:
-            raise SeederCommandError('You must provide an app to seed')
+    def handle(self, *app_labels, **options):
+        number = options.get('number')
+        seed_all = options.get('seed_all')
 
-        try:
-            number = int(options['number'])
-        except ValueError:
-            raise SeederCommandError('The value of --number must be an integer')
+        # Collect all models
+        self.models = []
+
+        if len(app_labels) == 0:
+            if seed_all:  # Add all models from all apps
+                for app_config in apps.get_app_configs():
+                    for model in app_config.get_models():
+                        self.add_model(model)
+            else:
+                raise SeederCommandError("You must specify a list of apps or the --all flag")
+        else:
+            for label in app_labels:
+                try:
+                    app_label, model_label = label.split('.')
+                except ValueError:
+                    # This is just an app - no model qualifier
+                    app_label = label
+                    try:
+                        app_config = apps.get_app_config(app_label)
+                    except LookupError:
+                        raise SeederCommandError("Unknown application: %s" % app_label)
+                    for model in app_config.get_models():
+                        self.add_model(model)
+                else:
+                    # app.model specified
+                    try:
+                        app_config = apps.get_app_config(app_label)
+                    except LookupError:
+                        raise SeederCommandError("Unknown application: %s" % app_label)
+                    try:
+                        model = app_config.get_model(model_label)
+                    except LookupError:
+                        raise SeederCommandError("Unknown model: %s.%s" % (app_label, model_label))
+                    self.add_model(model)
 
         seeder = Seed.seeder()
 
         # don't diplay warnings about non-timezone aware datetimes
         import warnings
+
         warnings.showwarning = lambda *x: None
 
-        for model in app_config.get_models():
+        for model in self.models:
             seeder.add_entity(model, number)
             print('Seeding %i %ss' % (number, model.__name__))
 
         pks = seeder.execute()
         print(pks)
-
