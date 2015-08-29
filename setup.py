3168d5ede3311bf88cea88af9da4059620465ab0
Chang-Hung Liang <eliang.cs@gmail.com>
2015-08-29 20:47:06 +0800

    Fix #10: Can't install django-seed without installing Django first

--- setup.py
+++ setup.py
@@ -1,9 +1,33 @@
+import codecs
 import os
+import re
 from setuptools import setup
+
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+# Read the version number from a source file.
+# Why read it, and not import?
+# https://packaging.python.org/en/latest/single_source_version.html
+def find_version(*file_paths):
+    # Open in Latin-1 so that we avoid encoding errors.
+    # Use codecs.open for Python 2 compatibility
+    with codecs.open(os.path.join(here, *file_paths), 'r', 'latin1') as f:
+        version_file = f.read()
+
+    # The version line must have the form
+    # __version__ = 'ver'
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError('Unable to find version string')
+
 
 setup(
     name='django-seed',
-    version=__import__('django_seed').__version__,
+    version=find_version('django_seed', '__init__.py'),
     author='Tobin Brown',
     author_email='tobin@brobin.me',
     packages=['django_seed', 'django_seed.management', 'django_seed.management.commands'],
