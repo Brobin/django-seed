6fb05a406a07fdc334cb4a8a25c8a86d86d09ea6
Lucas Moeskops <lucas@fabrique.nl>
2018-12-05 10:50:00 +0100

    Ignore DST on random date time objects since they might be ambiguous or non existent.

--- django_seed/guessers.py
+++ django_seed/guessers.py
@@ -15,7 +15,9 @@ def _timezone_format(value):
     :param value: The datetime value
     :return: A locale aware datetime
     """
-    return timezone.make_aware(value, timezone.get_current_timezone()) if getattr(settings, 'USE_TZ', False) else value
+    if getattr(settings, 'USE_TZ', False):
+        return timezone.make_aware(value, timezone.get_current_timezone(), is_dst=False)
+    return value
 
 
 class NameGuesser(object):
